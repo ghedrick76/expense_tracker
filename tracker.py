a38201b562f3256125d5c4305a0665c1feb4f3c8
ghedrick76 <graysonh76@icloud.com>
2022-11-14 21:10:15 -0500

    added expensetracker class and main()

--- tracker.py
+++ tracker.py
@@ -1,7 +1,25 @@
-import datetime
-import sqlite3
-from tkcalendar import DateEntry
-
+import db
 from tkinter import *
-import tkinter.messagebox as mb
-import tkinter.ttk as ttk
+from tkinter.ttk import *
+
+LARGE_FONT = ("Albertus Medium", 32)
+
+class ExpenseTracker:
+    def __init__(self, master):
+        self.frame = Frame(master)
+        self.frame.pack()
+        self.main_window()
+
+        #grid, labels, and buttons to be added
+
+# The main function to be added at the end of the script
+def main():
+    #db.create_tables(connection)
+    root = Tk()
+    root.gemoetry('250x200')
+    root.title("Expense Tracking Application")
+    tracker = ExpenseTracker(root)
+
+    root.mainloop()
+
+main()
