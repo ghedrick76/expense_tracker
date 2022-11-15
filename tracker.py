5fd074970133ea2631db74fedc7f606b0d1af9f9
ghedrick76 <graysonh76@icloud.com>
2022-11-14 21:54:07 -0500

    added buttons, added functionality

--- tracker.py
+++ tracker.py
@@ -42,7 +42,7 @@ class ExpenseTracker:
         delete = database(expense, cost)
         return delete
 
-    # Builds main window with each expense type
+    # Builds main window with each button
     def main_window(self):
         button1 = Button(self.frame, text="Fixed Expenses", command=self.fixed)
         button1.pack()
@@ -50,11 +50,18 @@ class ExpenseTracker:
         button2 = Button(self.frame, text="Recurring Expenses", command=self.recurring)
         button2.pack()
 
-        button2 = Button(self.frame, text="Non-recurring Expenses", command=self.nonrecurring)
-        button2.pack()
+        button3 = Button(self.frame, text="Non-recurring Expenses", command=self.nonrecurring)
+        button3.pack()
 
-        button2 = Button(self.frame, text="Extraneous Expenses", command=self.extraneous)
-        button2.pack()
+        button4 = Button(self.frame, text="Extraneous Expenses", command=self.extraneous)
+        button4.pack()
+
+        button5 = Button(self.frame, text="Analysis", command=self.analysis)
+        button5.pack()
+
+        button6 = Button(self.frame, text="Exit", command=exit)
+        button6.pack()
+        
 
 
 
@@ -64,16 +71,50 @@ class ExpenseTracker:
 
 
     def fixed(self):
-        top = TopLevel(self.frame)
+        top = Toplevel(self.frame)
+        top.title('Fixed Expenses')
+        l1 = Label(top, text="Expense Type").grid(row = 1, column = 0, sticky = W, pady = 2)
+        l2 = Label(top, text="Cost").grid(row = 2, column = 0, sticky = W, pady = 2)
+        l3 = Label(top, text="Date of Expense").grid(row = 3, column = 0, sticky = W, pady = 2)
+
+        e1 = Entry(top)
+        e1.grid(row=1, column=1, sticky=W, pady=2)
+        e2 = Entry(top)
+        e2.grid(row=2, column=1, sticky=W, pady=2)
+        e3 = Entry(top)
+        e3.grid(row=3, column=1, sticky=W, pady=2)
+
+        text = Text(top, width=40, height=10)
+        text.grid(row=5, column=1, columnspan=2)
+
+        #BUTTONS###
+
+        B1 = Button(top, text="Insert Values", command=lambda: (self.insert(db.insert_groceries,e1,e2,e3), self.added(top)))
+        B1.grid(row=1, column=2)
+
+        B2 = Button(top, text="Select All", command=lambda: (text.delete(1.0, END), text.insert(END, self.display_all(db.select_all_groceries()))))
+        B2.grid(row=2, column=2)
+
+        B3 = Button(top, text="Find value", command=lambda: (text.delete(1.0, END), text.insert(END, self.find_expense(db.select_grocery, e1,e2))))
+        B3.grid(row=2, column=3)
+
+        B3 = Button(top, text="Delete expense", command=lambda: (self.delete_expense(db.delete_grocery, e1,e2), self.delete(top)))
+        B3.grid(row=4, column=2)
+
+        B5= Button(top, text="Exit", command=exit)
+        B5.grid(row=4, column=3)
 
     def recurring(self):
-        top = TopLevel(self.frame)
+        top = Toplevel(self.frame)
 
     def nonrecurring(self):
-        top = TopLevel(self.frame)
+        top = Toplevel(self.frame)
 
     def extraneous(self):
-        top = TopLevel(self.frame)
+        top = Toplevel(self.frame)
+
+    def analysis(self):
+        top = Toplevel(self.frame)
 
 
 
